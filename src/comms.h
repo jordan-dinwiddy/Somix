001799e621a781f0f46c1e814b438516a296bbba
Jo Dinwiddy <jo.dinwiddy@gmail.com>
2010-04-01 17:06:42 +0100

    Adding info verbosity levels.

--- src/comms.h
+++ src/comms.h
@@ -2,6 +2,12 @@
 
 //#define DEBUG
 
+/* Info levels. Getting more verbose the higher we go.
+ * More important messages will be recorded at lower info levels.
+ */
+#define INFO_LEVEL_1
+//#define INFO_LEVEL_2
+
 #ifdef DEBUG
 #define debug(format, args...) printf(format "\n", ##args);
 #else
@@ -9,7 +15,18 @@
 #endif
 
 
+#ifdef INFO_LEVEL_1
+#define info_1(format, args...) printf("INFO: "); printf(format "\n", ##args);
+#else
+#define info_1(format, args...)
+#endif
+
+#ifdef INFO_LEVEL_2
 #define info(format, args...) printf("INFO: "); printf(format "\n", ##args);
+#else
+#define info(format, args...)
+#endif
+
 /**
  * For fatal error messages issued.
  */
