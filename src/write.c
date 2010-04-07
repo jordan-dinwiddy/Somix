2cdff05989b5805c1b386781181b4f06d625126d
Jo Dinwiddy <jo.dinwiddy@gmail.com>
2010-04-07 16:56:52 +0100

    Fixed bug in write.c:dir_add() that resulted in blocks not being put and therefore there blk_count increasing much higher ought to be. Eventually, on heavy usage, blk_count overflowed and on subsequent put_block calls somix would panic because it appeared to be putting a block that was not in use.

--- src/write.c
+++ src/write.c
@@ -46,6 +46,7 @@ static int dir_add(struct minix_inode *p_dir, const char *filename,
 			new_slots++;
 		}
 		if(found_slot) break;
+		put_block(block, DIR_BLOCK);		/* release dir block */
 		c_pos += BLOCK_SIZE;
 	}
 
@@ -54,7 +55,7 @@ static int dir_add(struct minix_inode *p_dir, const char *filename,
   		 * hold our extra data. */
 		debug("dir_add(%d, \"%s\", %d): no free slots in directory. "
 			"extending...", p_dir->i_num, filename, i_num);
-		if(block != NULL) put_block(block, DIR_BLOCK);	
+		
 		if((block = new_block(p_dir, p_dir->i_size)) == NULL)
 			panic("dir_add(...): unable to extend directory");
 			
